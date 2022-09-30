2ff4471c5271d1fee6baa9ac02dc8d0edbaa4b0c
BuildTools <unconfigured@null.spigotmc.org>
2022-09-30 21:41:13 +0100

    WEBSITE UPDATE

---
+++ editor.py
@@ -0,0 +1,29 @@
+import os
+import time
+import keyboard
+
+isOpen = True
+started = False
+mtime = os.path.getmtime("index.html")
+
+def openLiveEditor():
+    global started
+    started = True
+    mtime = os.path.getmtime("index.html")
+    os.startfile("index.html")
+
+def closeLiveEditor():
+    global started
+    started = True
+    os.startfile("index.html")
+
+openLiveEditor()
+
+while started:
+    if(mtime != os.path.getmtime("index.html")):
+        os.startfile("update.bat")
+        print('Updating...')
+    if keyboard.is_pressed('ctrl+alt+q'):
+        print('Done Editing')
+        break
+    print("Waiting for changes...")
--- update.bat
+++ update.bat
@@ -1,4 +1,3 @@
 git add --all
 git commit -m "WEBSITE UPDATE"
 git push -u origin main
-pause
